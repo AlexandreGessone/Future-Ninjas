11398d13cf1c3543df51c317ee326bff3397fefa
Alexandre Gessone <alex.gessone@gmail.com>
2020-07-24 15:03:01 -0300

    envio para o git

---
+++ mello-futureNinjas-2/build/precache-manifest.f358c9b94605ec4c587898e746b0460c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "902c62d432b800496d83d7366c0c43c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efe2b8b1ba7a03bc3102",
+    "url": "/static/css/main.8539cd08.chunk.css"
+  },
+  {
+    "revision": "bb9eea9e333d3fd59039",
+    "url": "/static/js/2.7d338dc1.chunk.js"
+  },
+  {
+    "revision": "a8cb445ba431d17b74d1d8ef8e67be2b",
+    "url": "/static/js/2.7d338dc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efe2b8b1ba7a03bc3102",
+    "url": "/static/js/main.f4531543.chunk.js"
+  },
+  {
+    "revision": "ff3cf58ab2f572376150",
+    "url": "/static/js/runtime-main.5c52f0c5.js"
+  },
+  {
+    "revision": "7511b9e4e8a6cd1c088a043e6fc249e3",
+    "url": "/static/media/logo.7511b9e4.svg"
+  },
+  {
+    "revision": "3b236f44a20f1b1d7292abfbe6c2fbea",
+    "url": "/static/media/logoBIG.3b236f44.svg"
+  }
+]);
